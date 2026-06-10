11816e8d8409a019e966712498a59aae6a88f275
Harshada <harshadapatil8289@gmail.com>
2026-06-10 20:01:19 +0530

    JavaScript Variable Practice

---
+++ js_practice/js_variable/variables.js
@@ -0,0 +1,18 @@
+"use strict";
+// console.log("hello World!!!");
+
+// #############   Introduction to Variables. ##################
+
+// Declare a Variable.
+var firstname;
+
+// 1. Variables can store some information.
+firstname = "Harshada Patil";
+
+// 2. We can use that information later.
+console.log(firstname);
+
+// 3. We can Change that information as well.
+firstname = "Harshada";
+console.log(firstname);
+
