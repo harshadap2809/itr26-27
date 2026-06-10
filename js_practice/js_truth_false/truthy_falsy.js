ae332bae22adb16eed090b360c72d53828e90fe2
Harshada <harshadapatil8289@gmail.com>
2026-06-10 20:56:59 +0530

    JavaScript Truth/false Practice

---
+++ js_practice/js_truth_false/truthy_falsy.js
@@ -0,0 +1,25 @@
+// ################## Truthy and falsy values ######################
+
+
+// #1. falsy values
+
+// false
+// ""
+// null
+// undefined
+// 0
+
+// 2# .truthy values
+// all are truthy
+// "abc"
+// 1, -1
+
+
+// try all falsy values and truthy to see output
+
+let firstname="Harshada";
+if(firstname){
+    console.log(firstname);
+}else{
+    console.log("empty variable");
+}
