228c9cac70b1662e9912a1202a1a87ed976b3896
Harshada <harshadapatil8289@gmail.com>
2026-06-10 20:27:24 +0530

    JavaScript String Methods Practice

---
+++ js_practice/js_stringMethods/string_concatenation.js
@@ -0,0 +1,25 @@
+// ########## String Concatenation ###############
+// adding two strings
+
+let string1 = "Harshada";
+let str2 = "Patil";
+
+let full_name = string1 + " " + str2;
+console.log(full_name);  
+
+// Assignment Guess output
+
+let str1 = 17;
+let str3 = 10;
+
+let output = str1 + str3;
+console.log(output);  //1710
+
+
+// Assignment Guess output
+
+// let str1 = "17";
+// let str3 = "10";
+
+// let output = +str1 + +str3;
+// console.log(output);  // 2
